f5bcc8546d7001c1eec852763433fc7a18607b90
Shivom <shivompaudel@icloud.com>
2022-10-17 13:57:39 -0500

    centered tweets and ordered tweets

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -2,6 +2,7 @@ import { Avatar, Button } from '@mui/material'
 import React, { useState } from 'react'
 import './css/tweetbox.css'
 import db from './firebase'
+import firebase from 'firebase/compat/app'
 
 function TweetBox() {
     const [tweetMessage, setTweetMessage] = useState('')
@@ -16,6 +17,7 @@ function TweetBox() {
             verified: true,
             text: tweetMessage, 
             image: tweetImage,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
             avatar: "https://shivomruns.weebly.com/uploads/1/2/1/4/121457582/img-1481_orig.jpg"
         })
 
